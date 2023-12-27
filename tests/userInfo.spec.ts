19d92e0e9ea37f4155a761961866322741a8c380
kwade <kwade@surgeforward.com>
2023-12-27 11:11:36 -0500

    Fix tests

--- tests/userInfo.spec.ts
+++ tests/userInfo.spec.ts
@@ -1,12 +1,14 @@
 import { test, expect } from '@playwright/test';
 
-test('has title based on subdomain', async ({ page }) => {
+test('office.wsplanningpoker.com has correct title based on subdomain', async ({ page }) => {
   //go to Office subdomain
   await page.goto('https://office.wsplanningpoker.com');
   
   //page title should be Office Planning Poker
   await expect(page).toHaveTitle('Office Planning Poker');
+});
 
+test('default.wsplanningpoker.com has correct title based on subdomain', async ({ page }) => {
   //go to Default subdomain
   await page.goto('https://default.wsplanningpoker.com');
   
